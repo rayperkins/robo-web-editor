00bfa3ebf2ff155bd756beb368596cd463ac4038
Ray Perkins <ray.perkins@consignlyhq.com>
2025-05-09 22:43:05 +1200

    add opcodes and for loop example

--- arduino/otto-blocky-pico/code_interpreter.h
+++ arduino/otto-blocky-pico/code_interpreter.h
@@ -11,11 +11,22 @@
 #define INSTRUCTION_HASARG(interpreter, opcode, instruction, length) (interpreter.hasArg(opcode, sizeof(opcode), instruction, length) == true)
 #define INSTRUCTION_GETARG(interpreter, opcode, instruction, length) (interpreter.getArg(opcode, sizeof(opcode), instruction, length))
 
-#define OPCODE_speed "speed"
-#define OPCODE_stop "stop"
-#define OPCODE_forward "forward"
-#define OPCODE_backward "backward"
+#define _ISOPCODE(opcode, instruction, length) (isOpcode(opcode, sizeof(opcode), instruction, length) == true)
+#define _HASARG(opcode, instruction, length) (hasArg(opcode, sizeof(opcode), instruction, length) == true)
+#define _GETARG(opcode, instruction, length) (getArg(opcode, sizeof(opcode), instruction, length))
 
+#define OPCODE_exit "exit"
+#define OPCODE_use "use"
+#define OPCODE_stor "stor"
+#define OPCODE_load "load"
+#define OPCODE_jump "jump"
+#define OPCODE_jmpe "jmpe"
+#define OPCODE_jmpn "jmpn"
+#define OPCODE_jmpp "jmpp"
+#define OPCODE_add "add"
+#define OPCODE_sub "sub"
+#define OPCODE_div "div"
+#define OPCODE_mul "mul"
 
 class CodeInterpreter
 {
@@ -28,6 +39,18 @@ public:
 
   void clearInstructions() {
     _instructionCount = 0;
+  }
+
+  int16_t getVariable(int index) {
+    return _variables[index];
+  }
+
+  void setVariable(int index, int16_t value) {
+    _variables[index] = value;
+  }
+
+  bool hasExited() {
+    return _instructionIndex < _instructionCount ? false : true;
   }
 
   void setInstruction(int index, const char * instruction, char length) {
@@ -92,13 +115,90 @@ public:
   }
 
   void step() {
-    if(_instruction < _instructionCount)
+    if(_instructionIndex < _instructionCount)
     {
-      char * instruction = _instructions[_instruction];
+      char * instruction = _instructions[_instructionIndex];
       char length = instruction[INSTRUCTION_SIZE]; // convention to store length at end
 
       if(length == 0) {
         // do nothing this step
+      }
+      else if (_ISOPCODE(OPCODE_exit, instruction, length)) {
+        _instructionIndex = _instructionCount;
+      }
+      else if (_ISOPCODE(OPCODE_use, instruction, length)) {
+        if(_HASARG(OPCODE_use, instruction, length)) {
+          _current = _GETARG(OPCODE_use, instruction, length);
+        }
+      }
+      else if (_ISOPCODE(OPCODE_stor, instruction, length)) {
+        if(_HASARG(OPCODE_stor, instruction, length)) {
+          int arg = _GETARG(OPCODE_stor, instruction, length);
+          _variables[arg] = _current;
+        }
+      }
+      else if (_ISOPCODE(OPCODE_load, instruction, length)) {
+        if(_HASARG(OPCODE_load, instruction, length)) {
+          int arg = _GETARG(OPCODE_load, instruction, length);
+          _current = _variables[arg];
+        }
+      }
+      else if (_ISOPCODE(OPCODE_jump, instruction, length)) {
+        if(_HASARG(OPCODE_jump, instruction, length)) {
+          int arg = _GETARG(OPCODE_jump, instruction, length);
+          _instructionIndex = arg < 0 ? 0 : (arg - 1);
+        }
+      }
+      else if (_ISOPCODE(OPCODE_jmpe, instruction, length)) {
+        // jump if current is 0 (ie, the result of comparing)
+        if(_current == 0) {
+          if(_HASARG(OPCODE_jmpe, instruction, length)) {
+            int arg = _GETARG(OPCODE_jmpe, instruction, length);
+            _instructionIndex = arg < 0 ? 0 : (arg - 1);
+          }
+        }
+      }
+      else if (_ISOPCODE(OPCODE_jmpn, instruction, length)) {
+        // jump if current is negative (ie, the result of comparing)
+        if(_current < 0) {
+          if(_HASARG(OPCODE_jmpn, instruction, length)) {
+            int arg = _GETARG(OPCODE_jmpn, instruction, length);
+            _instructionIndex = arg < 0 ? 0 : (arg - 1);
+          }
+        }
+      }
+      else if (_ISOPCODE(OPCODE_jmpp, instruction, length)) {
+        // jump if current is positive (ie, the result of comparing)
+        if(_current > 0) {
+          if(_HASARG(OPCODE_jmpp, instruction, length)) {
+            int arg = _GETARG(OPCODE_jmpp, instruction, length);
+            _instructionIndex = arg < 0 ? 0 : (arg - 1);
+          }
+        }
+      }
+      else if (_ISOPCODE(OPCODE_add, instruction, length)) {
+        if(_HASARG(OPCODE_add, instruction, length)) {
+          int arg = _GETARG(OPCODE_add, instruction, length);
+          _current += arg;
+        }
+      }
+      else if (_ISOPCODE(OPCODE_sub, instruction, length)) {
+        if(_HASARG(OPCODE_sub, instruction, length)) {
+          int arg = _GETARG(OPCODE_sub, instruction, length);
+          _current -= arg;
+        }
+      }
+      else if (_ISOPCODE(OPCODE_div, instruction, length)) {
+        if(_HASARG(OPCODE_div, instruction, length)) {
+          int arg = _GETARG(OPCODE_div, instruction, length);
+          _current /= arg;
+        }
+      }
+      else if (_ISOPCODE(OPCODE_mul, instruction, length)) {
+        if(_HASARG(OPCODE_mul, instruction, length)) {
+          int arg = _GETARG(OPCODE_mul, instruction, length);
+          _current *= arg;
+        }
       }
       else if(handle_instruction(instruction, length)) {
         
@@ -116,12 +216,12 @@ public:
       //   // walk backwards
       // }
 
-      _instruction++;
+      _instructionIndex++;
     }
   }
 
 private:
-  int _instruction = 0;
+  int _instructionIndex = 0;
   int _current = 0;
   int _variableCount = 0;
   int16_t _variables[VARIABLE_LIST_SIZE];
--- arduino/tests/test.cpp
+++ arduino/tests/test.cpp
@@ -1,6 +1,8 @@
 #include <catch2/catch_test_macros.hpp>
 #include "code_interpreter.h"
 
+#define SETINSTRUCTION(interpreter, index, instruction) interpreter.setInstruction(index, instruction, sizeof(instruction))
+
 class TestCodeInterpreter : public CodeInterpreter {
 public:
     int stepCount = 0;
@@ -17,7 +19,7 @@ public:
 
 };
 
-TEST_CASE( "clearInstruction", "[Ottointerpreter]" ) {
+TEST_CASE( "clearInstruction", "[CodeInterpreter]" ) {
     TestCodeInterpreter interpreter;
     interpreter.setInstruction(0, "forward", sizeof("forward"));
     interpreter.clearInstructions();
@@ -25,7 +27,7 @@ TEST_CASE( "clearInstruction", "[Ottointerpreter]" ) {
     REQUIRE( interpreter.getInstructionCount() == 0 );
 }
 
-TEST_CASE( "setInstruction", "[Ottointerpreter]" ) {
+TEST_CASE( "setInstruction", "[CodeInterpreter]" ) {
     TestCodeInterpreter interpreter;
     interpreter.setInstruction(0, "forward", sizeof("forward"));
     interpreter.setInstruction(1, "backward", sizeof("backward"));    
@@ -34,7 +36,7 @@ TEST_CASE( "setInstruction", "[Ottointerpreter]" ) {
     REQUIRE( interpreter.getInstructionCount() == 3 );
 }
 
-TEST_CASE( "step", "[Ottointerpreter]" ) {
+TEST_CASE( "step", "[CodeInterpreter]" ) {
     TestCodeInterpreter interpreter;
     interpreter.setInstruction(0, "forward", sizeof("forward"));
     interpreter.setInstruction(1, "backward", sizeof("backward"));    
@@ -50,47 +52,76 @@ TEST_CASE( "step", "[Ottointerpreter]" ) {
     REQUIRE( interpreter.stepCount == 3 );
 }
 
-TEST_CASE( "isOpcode", "[Ottointerpreter]" ) {
+TEST_CASE( "isOpcode", "[CodeInterpreter]" ) {
     TestCodeInterpreter interpreter;
     REQUIRE( interpreter.isOpcode("forward", sizeof("forward"), "forward 123", sizeof("forward 123")) == true );
 }
 
-TEST_CASE( "isOpcode is false", "[Ottointerpreter]" ) {
+TEST_CASE( "isOpcode is false", "[CodeInterpreter]" ) {
     TestCodeInterpreter interpreter;
     REQUIRE( interpreter.isOpcode("notforward", sizeof("notforward"), "forward 123", sizeof("forward 123")) == false );
 }
 
-TEST_CASE( "hasArg", "[Ottointerpreter]" ) {
+TEST_CASE( "hasArg", "[CodeInterpreter]" ) {
     TestCodeInterpreter interpreter;
     REQUIRE( interpreter.hasArg("forward", sizeof("forward"), "forward 123", sizeof("forward 123")) == true );
 }
 
-TEST_CASE( "hasArg is false", "[Ottointerpreter]" ) {
+TEST_CASE( "hasArg is false", "[CodeInterpreter]" ) {
     TestCodeInterpreter interpreter;
     REQUIRE( interpreter.hasArg("forward", sizeof("forward"), "forward", sizeof("forward")) == false );
 }
 
-TEST_CASE( "getArg", "[Ottointerpreter]" ) {
+TEST_CASE( "getArg", "[CodeInterpreter]" ) {
     TestCodeInterpreter interpreter;
     REQUIRE( interpreter.getArg("forward", sizeof("forward"), "forward 123", sizeof("forward 123")) == 123 );
 }
 
-TEST_CASE( "getArg is negative", "[Ottointerpreter]" ) {
+TEST_CASE( "getArg is negative", "[CodeInterpreter]" ) {
     TestCodeInterpreter interpreter;
     REQUIRE( interpreter.getArg("forward", sizeof("forward"), "forward -123", sizeof("forward -123")) == -123 );
 }
 
-TEST_CASE( "INSTRUCTION_ISOPCODE", "[Ottointerpreter]" ) {
+TEST_CASE( "INSTRUCTION_ISOPCODE", "[CodeInterpreter]" ) {
     TestCodeInterpreter interpreter;
     REQUIRE( INSTRUCTION_ISOPCODE(interpreter, "forward", "forward 123", sizeof("forward 123")) == true );
 }
 
-TEST_CASE( "INSTRUCTION_HASARG", "[Ottointerpreter]" ) {
+TEST_CASE( "INSTRUCTION_HASARG", "[CodeInterpreter]" ) {
     TestCodeInterpreter interpreter;
     REQUIRE( INSTRUCTION_HASARG(interpreter, "forward", "forward 123", sizeof("forward 123")) == true );
 }
 
-TEST_CASE( "INSTRUCTION_GETARG", "[Ottointerpreter]" ) {
+TEST_CASE( "INSTRUCTION_GETARG", "[CodeInterpreter]" ) {
     TestCodeInterpreter interpreter;
     REQUIRE( INSTRUCTION_GETARG(interpreter, "forward", "forward 123", sizeof("forward 123")) == 123 );
+}
+
+// for loop
+TEST_CASE( "for loop", "[CodeInterpreter]" ) {
+    TestCodeInterpreter interpreter;
+    // example of a for loop, ie: for( i = 0; i < 10; i++)
+    SETINSTRUCTION(interpreter, 0, "use 0");
+    SETINSTRUCTION(interpreter, 1, "stor 1"); // store 0 to variable 1
+    SETINSTRUCTION(interpreter, 2, "stor 0"); // store 0 to variable 0
+    SETINSTRUCTION(interpreter, 3, "sub 10"); // prev result negative if loop not done
+    SETINSTRUCTION(interpreter, 4, "jmpe 11"); // jump to end if 0, ie loop is done
+    SETINSTRUCTION(interpreter, 5, "load 0"); // increment variable 1
+    SETINSTRUCTION(interpreter, 6, "add 1"); // increment variable 1
+    SETINSTRUCTION(interpreter, 7, "stor 1");
+    SETINSTRUCTION(interpreter, 8, "load 0"); // end of loop body, increment variable 0
+    SETINSTRUCTION(interpreter, 9, "add 1"); 
+    SETINSTRUCTION(interpreter, 10, "jump 2");
+    SETINSTRUCTION(interpreter, 11, "exit");
+
+    int stepCount = 0;
+
+    while(interpreter.getVariable(1) < 10
+        && !interpreter.hasExited()
+        && stepCount < 100) {
+        interpreter.step();
+        stepCount++;
+    }
+
+    REQUIRE( interpreter.getVariable(1) == 10 );
 }
